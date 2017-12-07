1d806471409104b8b385cf3806a2b75c7f4227d1
Finrod-Felagund <31896424+Finrod-Felagund@users.noreply.github.com>
2017-12-07 21:10:45 +0300

    Basic Python Scripts
    MswMvlan.py : Configure Vlans via loop and subloop on multipule switches.
    
    MswithcallFile: The script will call a file
    -filename-* for ip address
    
    Swconf.py: Basic fast config script for
     all switches(call file for ips).
    
    deletevlans: deletes the vlan.dat
    
    Defaultscript: used for show commands, etc.

---
+++ DefaultScript.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import getpass
+import sys
+import telnetlib
+
+#The script will call a file devicesips for ip address, in this case I'm using the same username and password on the switches, I'm only goona be prompted for that once#
+#If however the cridintials were difirent on each switch, It's better to add the authentication prompt as part of the loop#
+
+
+user = raw_input("Username: ")  #enter username once for all targeted switches
+password = getpass.getpass()    #enter password once for all targeted switches
+
+f = open ("devicesips")    #open file named devicesips
+
+for line in f:  #Ip address called from the file#
+    print "Configuring Switch " + (line)
+    HOST = line #Telnet to hosts in devicesips#
+    tn = telnetlib.Telnet(HOST)
+
+    tn.read_until("Username: ") #Loop asks for a username#
+    tn.write(user + "\n")       #Pass the user variable defined at start of the script#
+    if password:
+        tn.read_until("Password: ")
+        tn.write(password +"\n")        #Pass the password variable defined at the startof the script#
+
+    tn.write("conf t\n")
+    tn.write("line cons 0\n")
+    tn.write("privil level 15\n")
+    tn.write("exit\n")
+
+    tn.write("end\n")
+    tn.write("wr\n")
+    tn.write("exit\n")
+
+    print tn.read_all()
---
+++ deletevlans.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import getpass
+import sys
+import telnetlib
+
+#The script will call a file devicesips for ip address, and delete vlan.dat file#   
+#After script executin, a reload from console line is a MUST#                                                     
+#Same credentials on all switches#
+
+user = raw_input("Username: ")  #enter username once for all targeted switches
+password = getpass.getpass()    #enter password once for all targeted switches
+
+f = open ("devicesips")    #open file named devicesips
+
+for line in f:  #Ip address called from the file#
+    print "Configuring Switch " + (line)
+    HOST = line #Telnet to hosts in devicesips#
+    tn = telnetlib.Telnet(HOST)
+
+    tn.read_until("Username: ") #Loop asks for a username#
+    tn.write(user + "\n")       #Pass the user variable defined at start of the script#
+    if password:
+        tn.read_until("Password: ")
+        tn.write(password +"\n")    #Pass the password variable defined at the startof the script#
+
+
+    tn.write("delete vlan.dat\n")
+    tn.write("\n")
+    tn.write("\n")
+
+    tn.write("wr\n")
+
+    tn.write("reload\n")    #RELOAD ONLY ALLOWED FROM CONSOLE LINE!!#
+    tn.write("\n")
+
+    tn.write("exit\n")
+
+    print tn.read_all()
