4932242171c1071d143af89993345fd2d8ab1870
Finrod-Felagund <31896424+Finrod-Felagund@users.noreply.github.com>
2018-01-21 22:00:04 +0300

    Basic Python Scripts (Telnet)
    1-Multi.Swit.Mult.Vlans : Configure Vlans via loop and subloop on multipule switches.
    
    2-MswithcallFile: The script will call a file -filename-* for ip address
    
    3-Swconf.py: Basic fast config script for all switches(call file for ips).
    
    4-deletevlans: deletes the vlan.dat
    
    5-Defaultscript: used for show commands, etc.
    
    6-switchConfis.py: The script will pull the configurations of switches and save them to a file.

--- Getconfigs.py
+++ Getconfigs.py
@@ -3,20 +3,25 @@
 import getpass
 import telnetlib
 
+
 #The script will pull the configurations of switches and save them to a file#
+
 
 user = raw_input("Username: ")  #enter username once for all targeted switches
 password = getpass.getpass()    #enter password once for all targeted switches
 
+
 f = open ("devicesips")    #open file named devicesips,default option is read only#
 
 for line in f:    #Ip address called from the file#
+
     print "Getting running-config for " + (line)
-    HOST = line    #Telnet to hosts in devicesips#
+    HOST = line.strip()    #Telnet to hosts in devicesips# #strip used to delete all white spaces#
     tn = telnetlib.Telnet(HOST)    #tn is the variable for the telnet session#
 
     tn.read_until("Username: ") #Loop asks for a username#
     tn.write(user + "\n")       #Pass the user variable defined at start of the script#
+
     if password:
         tn.read_until("Password: ")
         tn.write(password +"\n")    #Pass the password variable defined at the startof the script#
@@ -25,10 +30,13 @@ for line in f:    #Ip address called from the file#
     tn.write("show run\n")
     tn.write("exit\n")
 
+
     readoutput = tn.read_all()    #This variable reads all output from telnet sessin(tn variable)#
-	
+
+
 #next script opens file(Switch+IP) and saves the running-configs to it#
-    saveoutput = open("Switch " + HOST, "w")    #here I need the variable to read/write, hence w#
+
+    saveoutput = open("ConfigsFor" + HOST, "w")    #here I need the variable to read/write, hence w# #make sure the file name contians NO spaces#
     saveoutput.write(readoutput)
     saveoutput.write("\n")
     saveoutput.close
